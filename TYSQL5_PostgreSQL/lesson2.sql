df9ee8889aaeddfb2902797873ebb562a79c03a4
Espinoso <espinoso.ian@gmail.com>
2025-08-18 12:01:21 -0300

    Finish exercises from lesson 2

---
+++ TYSQL5_PostgreSQL/lesson2.sql
@@ -0,0 +1,19 @@
+/* Exercise 1
+
+SELECT cust_id
+FROM Customers */
+
+
+/* Exercise 2
+
+SELECT DISTINCT prod_id
+FROM OrderItems */
+
+
+/* Exercise 3
+
+SELECT * -- Select everything
+FROM Customers
+
+SELECT cust_id -- Select only the client's id
+FROM Customers */
