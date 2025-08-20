0a884c8ca0814a5fa998888199d94f5f3934e0c2
Espinoso <espinoso.ian@gmail.com>
2025-08-20 09:06:04 -0300

    Finish exercises from chapter 4

---
+++ TYSQL5_PostgreSQL/lesson4.sql
@@ -0,0 +1,27 @@
+/* -- Exercise 1
+
+SELECT prod_id, prod_name
+FROM Products
+WHERE prod_price = 9.49; */
+
+
+/* -- Exercise 2
+
+SELECT prod_id, prod_name
+FROM Products
+WHERE prod_price >= 9; */
+
+
+/* -- Exercise 3
+
+SELECT DISTINCT order_num
+FROM OrderItems
+WHERE quantity >= 100; */
+
+
+/* -- Exercise 4
+
+SELECT prod_name, prod_price
+FROM Products
+WHERE prod_price BETWEEN 3 AND 6
+ORDER BY prod_price; */
