5cf21e19825dc43f01e2f7df4945f83c7b7148b8
Espinoso <espinoso.ian@gmail.com>
2025-08-23 11:58:47 -0300

    Finish lesson 11

--- TYSQL5_PostgreSQL/lesson11.sql
+++ TYSQL5_PostgreSQL/lesson11.sql
@@ -1,25 +1,51 @@
--- Exercise 1
+/* -- Exercise 1
 
 SELECT cust_id
 FROM Orders
 WHERE Orders.order_num IN (SELECT OrderItems.order_num
                            FROM OrderItems
-                           WHERE item_price >= 10);
+                           WHERE item_price >= 10); */
 
 
--- Exercise 2
+/* -- Exercise 2
+
+SELECT order_date, cust_id
+-- Orders has order_date and cust_id
+FROM Orders
+-- OrderItems and Orders are joinable by order_num
+WHERE order_num IN (SELECT order_num
+-- OrderItems has prod_id
+                    FROM OrderItems
+                    WHERE prod_id = 'BR01')
+ORDER BY order_date; */
 
 
+/* -- Exercise 3
+SELECT TRIM(cust_email) AS cust_email
+-- Customers contains cust_email
+FROM Customers
+-- Customers and Orders can be joined by cust_id
+WHERE cust_id IN (SELECT cust_id
+                  FROM Orders
+                  WHERE order_num IN (SELECT order_num
+	                                  FROM OrderItems
+	                                  WHERE prod_id = 'BR01')); */
 
 
--- Exercise 3
+/* -- Exercise 4
+
+SELECT cust_id,
+       (SELECT SUM(item_price * quantity)
+        FROM OrderItems
+        WHERE OrderItems.order_num = Orders.order_num) AS order_cost
+FROM Orders
+ORDER BY order_cost DESC; */
 
 
+/* -- Exercise 5
 
-
--- Exercise 4
-
-
-
-
--- Exercise 5
+SELECT TRIM(prod_name) AS product,
+      (SELECT SUM(quantity) 
+       FROM OrderItems
+       WHERE OrderItems.prod_id = Products.prod_id) AS quant_sold
+FROM Products; */
