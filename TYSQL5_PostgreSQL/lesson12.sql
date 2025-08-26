4a8cb63ab21f6b0b354c7342d3cf786a536e1278
Espinoso <espinoso.ian@gmail.com>
2025-08-26 16:06:48 -0300

    Finish lesson 12

--- TYSQL5_PostgreSQL/lesson12.sql
+++ TYSQL5_PostgreSQL/lesson12.sql
@@ -12,19 +12,70 @@ INNER JOIN Customers ON Orders.cust_id = Customers.cust_id
 ORDER BY cust_name, order_num; */
 
 
--- Exercise 2
+/* -- Exercise 2
 
-
+SELECT cust_name, Orders.order_num, SUM(quantity * item_price) AS OrderTotal
+FROM Orders
+INNER JOIN Customers 
+	ON Orders.cust_id = Customers.cust_id
+INNER JOIN OrderItems 
+	ON Orders.order_num = OrderItems.order_num
+GROUP BY cust_name, Orders.order_num
+ORDER BY OrderTotal DESC; */
 
 
 -- Exercise 3
 
-
+/* SELECT order_date, cust_id
+FROM Orders, OrderItems
+WHERE Orders.order_num = OrderItems.order_num
+AND prod_id = 'BR01'
+ORDER BY order_date; */
 
 
 -- Exercise 4
 
-
-
+/* SELECT TRIM(cust_email) AS cust_email
+FROM Customers
+INNER JOIN Orders ON Orders.cust_id = Customers.cust_id
+INNER JOIN OrderItems ON OrderItems.order_num = Orders.order_num
+WHERE prod_id = 'BR01'; */
 
 -- Exercise 5
+
+-- Using the ANSI-standard INNER JOIN
+/* SELECT cust_name, SUM(quantity * item_price) AS OrderTotal
+FROM Customers
+INNER JOIN Orders ON Orders.cust_id = Customers.cust_id
+INNER JOIN OrderItems ON OrderItems.order_num = Orders.order_num
+GROUP BY cust_name
+HAVING SUM(quantity * item_price) >= 1000
+ORDER BY OrderTotal DESC; */
+
+-- Using equijoin
+/* SELECT cust_name, SUM(quantity * item_price) AS OrderTotal
+FROM Customers, Orders, OrderItems
+WHERE Orders.cust_id = Customers.cust_id
+AND OrderItems.order_num = Orders.order_num
+GROUP BY cust_name
+HAVING SUM(quantity * item_price) >= 1000
+ORDER BY OrderTotal DESC; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
