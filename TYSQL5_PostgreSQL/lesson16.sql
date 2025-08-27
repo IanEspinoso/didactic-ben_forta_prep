05f5ac64bf6a2b1d1d1f92231ddecfa93e7302ef
Espinoso <espinoso.ian@gmail.com>
2025-08-27 11:59:25 -0300

    Finish lesson 16

---
+++ TYSQL5_PostgreSQL/lesson16.sql
@@ -0,0 +1,14 @@
+/*-- Exercise 1
+
+UPDATE Vendors
+SET vend_state = UPPER(vend_state)
+WHERE vend_country = 'USA';
+
+UPDATE Customers
+SET cust_state = UPPER(cust_state)
+WHERE cust_country = 'USA'; */
+
+/* -- Exercise 2
+
+DELETE FROM Customers
+WHERE cust_name = '1000000006'; */
